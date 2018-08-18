79512537016b3d6371c893a72189d6ecb646aa8e
Tyler Warrick <tcw5038@gmail.com>
2018-08-17 20:07:06 -0700

    Tried to fix issue

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/posts',  (req, res) => {
 
 app.get('/posts:id', (req, res) => {
   BlogPost.findById(req.params.id)
-    .then(restaurant => res.json(restaurant.serialize()))
+    .then(blogPost => res.json(blogPost.serialize()))
     .catch(err => {
       console.error(err);
       res.status(500).json({message: 'Internal server error'});
@@ -76,7 +76,6 @@ app.put('/posts:id', (req, res) => {
   });
 
   BlogPost
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
     .findByIdAndUpdate(req.params.id, { $set: toUpdate })
     .then(blogPost => res.status(204).end())
     .catch(err => res.status(500).json({ message: 'Internal server error' }));
@@ -88,17 +87,13 @@ app.delete('/posts:id', (req, res) =>{
     .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
-// catch-all endpoint if client makes request to non-existent endpoint
 app.use('*', function(req, res) {
   res.status(404).json({ message: 'Not Found' });
 });
 
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, so we declare `server` here
-// and then assign a value to it in run
+
 let server;
 
-// this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
     mongoose.connect(
@@ -137,8 +132,6 @@ function closeServer() {
   });
 }
 
-// if server.js is called directly (aka, with `node server.js`), this block
-// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
   runServer(DATABASE_URL).catch(err => console.error(err));
 }
